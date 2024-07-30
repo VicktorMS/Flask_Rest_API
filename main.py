8575c3350d851a7737397227630e6f7211ba725d
victormoraes <victor.moraesrj@gmail.com>
2024-07-30 18:46:59 -0300

    Usando Request Argument Parser para receber PUTs

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
+
+video_put_args = reqparse.RequestParser() # serve para definir argumentos obrigatórios para serem enviados
+video_put_args.add_argument(
+    'title', type=str, # tipo de argumento
+    help='Title of the video is required', # um espécie de mensagem de erro
+)
+video_put_args.add_argument(
+    'views', type=int, default=0
+)
+video_put_args.add_argument(
+    'likes', type=int, default=0
+)
 
 videos = {}
 
@@ -12,8 +24,8 @@ class Video(Resource):
         return videos[video_id]
     
     def put(self, video_id):
-        print(request.form['title'])
-        return 
+        args = video_put_args.parse_args()
+        return {video_id: args}
         
 
 api.add_resource(Video, '/video/<int:video_id>')
